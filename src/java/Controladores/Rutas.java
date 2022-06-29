4550a920f90cb4559539bb671cb583810147a263
Rolando del castillo <1612783@utp.edu.pe>
2022-06-28 23:33:05 -0500

    Prueba

--- src/java/Controladores/Rutas.java
+++ src/java/Controladores/Rutas.java
@@ -4,11 +4,7 @@
  */
 package Controladores;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
